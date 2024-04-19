950ace19f70a6f9599e24ffb12b63a8c68e6dfa3
marcocslima@gmail.com <marcocslima@gmail.com>
2024-04-18 22:22:34 -0300

    feat: modify code and add new features

--- Desafio_S_Banc_Simples/desafio.py
+++ Desafio_S_Banc_Simples/desafio.py
@@ -6,65 +6,147 @@ Digite a opção desejada:
 1 - Depositar valor
 2 - Sacar valor
 3 - Extrato
-4 - Sair do sistema
+4 - Cadastrar usuário
+5 - Listar usuários
+6 - Cadastrar conta
+7 - Sair do sistema
 
 '''
 
-opcao = 0
+estados_brasileiros = [
+    "AC",  # Acre
+    "AL",  # Alagoas
+    "AP",  # Amapá
+    "AM",  # Amazonas
+    "BA",  # Bahia
+    "CE",  # Ceará
+    "DF",  # Distrito Federal
+    "ES",  # Espírito Santo
+    "GO",  # Goiás
+    "MA",  # Maranhão
+    "MT",  # Mato Grosso
+    "MS",  # Mato Grosso do Sul
+    "MG",  # Minas Gerais
+    "PA",  # Pará
+    "PB",  # Paraíba
+    "PR",  # Paraná
+    "PE",  # Pernambuco
+    "PI",  # Piauí
+    "RJ",  # Rio de Janeiro
+    "RN",  # Rio Grande do Norte
+    "RS",  # Rio Grande do Sul
+    "RO",  # Rondônia
+    "RR",  # Roraima
+    "SC",  # Santa Catarina
+    "SP",  # São Paulo
+    "SE",  # Sergipe
+    "TO"   # Tocantins
+]
+
 saldo = 0
 historico_operacoes = []
-num_max_saques_diarios = 3
+usuarios = []
+NUM_MAX_SAQUES_DIA = 3
 LIMITE_SAQUE = 500
 
-def depositar(valor):
-    global saldo
+def registro(operacao, valor):
+    data_hora = datetime.datetime.now()
+    reg = f'{operacao} de R$ {valor:.2f} em {data_hora.strftime("%d/%m/%Y %H:%M:%S")}'
+    return reg
+
+def depositar(valor, saldo, /):
     if valor <= 0:
         print('Valor inválido')
         return
     saldo += valor
-    data_hora = datetime.datetime.now()
-    historico_operacoes.append(f'Depósito de R$ {valor:.2f} em {data_hora.strftime("%d/%m/%Y %H:%M:%S")}')
+    return saldo
 
-def sacar(valor):
-    global saldo
-    global num_max_saques_diarios
-    for s in historico_operacoes:
-        if 'Saque' in s:
+def sacar(*, valor, saldo, historico):
+    global NUM_MAX_SAQUES_DIA
+    for h in historico:
+        if 'Saque' in h:
             data_atual = datetime.datetime.now()
-            if data_atual.strftime('%d/%m/%Y') in s:
-                num_max_saques_diarios -= 1
-    if valor <= 0:
+            if data_atual.strftime('%d/%m/%Y') in h:
+                NUM_MAX_SAQUES_DIA -= 1
+    if valor <= 0.0:
         print('Valor inválido')
         return
     if valor > LIMITE_SAQUE:
         print(f'Valor máximo para saque é de R$ {LIMITE_SAQUE:.2f}')
         return
-    if num_max_saques_diarios < 0:
+    if NUM_MAX_SAQUES_DIA < 0.0:
         print('Limite de saques diários atingido')
         return
-    if saldo < valor or saldo == 0:
+    if saldo < valor or saldo == 0.0:
         print('Saldo insuficiente')
         return
     saldo -= valor
-    data_hora = datetime.datetime.now()
-    historico_operacoes.append(f'Saque de R$ {valor:.2f} em {data_hora.strftime("%d/%m/%Y %H:%M:%S")}')
+    return saldo
 
-def extrato():
+def extrato(historico_operacoes, /, *, saldo):
     for i in historico_operacoes:
         print(i)
     print(f'\nSaldo: R$ {saldo:.2f}\n')
 
-while opcao != 4:
+def criar_usuario(*, nome, data_nascimento, cpf, endereco):
+    usuario = {
+        'nome': nome,
+        'data_nascimento': data_nascimento,
+        'cpf': cpf,
+        'endereco': endereco,
+    }
+    return usuario
+
+def listar_usuarios():
+    for u in usuarios:
+        print(u)
+
+def montar_endereco(*, logradouro, numero, bairro, cidade, estado):
+    endereco = f'{logradouro}, {numero} - {bairro}, {cidade} - {estado}'
+    return endereco
+
+def verifica_sigla_estados(sigla):
+    if sigla not in estados_brasileiros:
+        print('Sigla de estado inválida')
+        return False
+    return True
+
+while True:
     opcao = int(input(menu))
     if opcao == 1:
-        valor = float(input('Digite o valor a ser depositado: '))
-        depositar(valor)
+        valor_input = float(input('Digite o valor da operação: '))
+        saldo = depositar(valor_input, saldo)
+        historico_operacoes.append(registro('Depósito', valor_input))
     elif opcao == 2:
-        valor = float(input('Digite o valor a ser sacado: '))
-        sacar(valor)
+        valor_input = float(input('Digite o valor da operação: '))
+        saldo = sacar(valor=valor_input, saldo=saldo, historico=historico_operacoes)
+        historico_operacoes.append(registro('Saque', valor_input))
     elif opcao == 3:
-        extrato()
+        extrato(historico_operacoes, saldo=saldo)
     elif opcao == 4:
+        nome = input('Digite o nome: ')
+        data_nascimento = input('Digite a data de nascimento: ')
+        cpf = input('Digite o CPF: ')
+        logradouro=input('Digite o logradouro: ')
+        numero=input('Digite o número: ')
+        bairro=input('Digite o bairro: ')
+        cidade=input('Digite a cidade: ')
+        while True:
+            sigla = input('Digite a sigla estado: ')
+            if verifica_sigla_estados(sigla):
+              estado=sigla 
+              break
+            else:
+              print('Digite uma sigla de estado válida!!!')
+              continue
+        endereco = montar_endereco(logradouro=logradouro, numero=numero, bairro=bairro, cidade=cidade, estado=estado)
+        usuario = criar_usuario(nome=nome, data_nascimento=data_nascimento, cpf=cpf, endereco=endereco)
+        usuarios.append(usuario)
+        print(usuario)
+    elif opcao == 5:
+        listar_usuarios()
+    elif opcao == 7:
         print('Saindo do sistema')
+        break
     else:
         print('Opção inválida')
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python_AI_bootcamp
+
+Repositório criado para os exercícios e desafios do python_AI_bootcamp.
